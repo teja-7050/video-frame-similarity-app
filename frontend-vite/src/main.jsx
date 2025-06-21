f22f1b42ced9b48779cea3a06e088688198c74d6
teja-7050 <tejasrisaikrishna2006@gmail.com>
2025-06-21 15:32:53 +0530

    main.jsx

--- frontend-vite/src/main.jsx
+++ frontend-vite/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // ✅ Works if App.jsx has `export default App`
+import App from "./App.jsx"; // 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
